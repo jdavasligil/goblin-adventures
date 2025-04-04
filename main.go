be24a3e014643705b1a5df3e59b0400f591bb90e
jdavasligil <22987230+jdavasligil@users.noreply.github.com>
2025-04-03 21:18:31 -0700

    Recieve token, send request

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 /*
 Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Modified 2025 J. Davasligil
 
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+this file except in compliance with the License. A copy of the License is
+located at
 
 	http://aws.amazon.com/apache2.0/
 
-	or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-
+	or in the "license" file accompanying this file. This file is distributed
+	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+	express or implied. See the License for the specific language governing
+	permissions and limitations under the License.
 */
 
 package main
@@ -14,8 +19,12 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 
 	"golang.org/x/oauth2/clientcredentials"
@@ -24,10 +33,13 @@ import (
 
 var (
 	clientID = "ui5s8b0hvlw4uz6fzd752kbvk6zipj"
-	// Consider storing the secret in an environment variable or a dedicated storage system.
 	clientSecret = ""
 	oauth2Config *clientcredentials.Config
 )
+
+type JSONResponse struct {
+	Data any `json:"data"`
+}
 
 func getEnvironmentVariables() map[string]string {
 	envVars := make(map[string]string)
@@ -37,8 +49,8 @@ func getEnvironmentVariables() map[string]string {
 		log.Fatalln("Fatal Error! Failure to read .env")
 	}
 
-	lines := bytes.Split(dat, []byte("\n"))
-	for _, line := range lines {
+	lines := bytes.SplitSeq(dat, []byte("\n"))
+	for line := range lines {
 		nameVar := bytes.Split(line, []byte("="))
 		if len(nameVar) == 2 {
 			envVars[string(nameVar[0])] = string(nameVar[1])
@@ -60,12 +72,41 @@ func main() {
 		ClientID:     clientID,
 		ClientSecret: clientSecret,
 		TokenURL:     twitch.Endpoint.TokenURL,
+		Scopes: []string{"user:read:chat", "user:write:chat"},
 	}
 
-	token, err := oauth2Config.Token(context.Background())
+	token, err := oauth2Config.Token(context.TODO())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Access token: %s\n", token.AccessToken)
+	fmt.Printf("Access Token:  %s\n", token.AccessToken)
+	fmt.Printf("Token Expires: %s\n", token.Expiry.Local().Truncate(1))
+
+	reqURL, err := url.Parse("https://api.twitch.tv/helix/users?login=twitchdev")
+	if err != nil {
+		log.Fatal(err)
+	}
+	getUserReq := &http.Request {
+		Method: "GET",
+		URL: reqURL,
+		Header: map[string][]string{
+			"Client-Id": {clientID},
+		},
+	}
+	token.SetAuthHeader(getUserReq)
+	response, err := http.DefaultClient.Do(getUserReq); if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	jsonResponse := &JSONResponse{}
+	err = json.Unmarshal(body, jsonResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print(jsonResponse)
 }
